1c56a3fecc196754043ed3407d6b72362b9156c0
Brian Reece <bdreece@mtu.edu>
2023-01-07 18:24:19 -0500

    Updated generate.sh

--- pkg/tools/generate.sh
+++ pkg/tools/generate.sh
@@ -3,7 +3,8 @@
 rm -rf $PROJECT_ROOT/**/*.pb.go $PROJECT_ROOT/pkg/proto/grpc
 mkdir -p $PROJECT_ROOT/pkg/proto/grpc
 
-protoc --proto_path=/usr/include \
+protoc --experimental_allow_proto3_optional \
+    --proto_path=/usr/include \
     --proto_path=$PROJECT_ROOT/pkg/proto \
     --go_out=$PROJECT_ROOT/pkg/proto \
     --go_opt="paths=source_relative" \
