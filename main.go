39252b084f5920736fb87bf138baa16251aa8365
Brian Reece <bdreece@mtu.edu>
2023-01-07 18:02:15 -0500

    Even more updates

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
-package main
+/*
+ * hopper - A gRPC API for collecting IoT device event messages
+ * Copyright (C) 2022 Brian Reece
+ 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+ package main
 
 import (
-	"log"
+	"os"
 
 	"github.com/bdreece/hopper/pkg/app"
 )
@@ -9,11 +27,11 @@ import (
 func main() {
 	a, err := app.NewApp()
 	if err != nil {
-		log.Fatal(err)
+		os.Exit(1)
 	}
 
 	err = a.Serve()
 	if err != nil {
-		log.Fatal(err)
+		os.Exit(1)
 	}
 }
